803a750300a71f81741348f44bd7ae208fdfd441
Geonchan Shin <pliton82@gmail.com>
2020-06-03 17:44:51 +0900

    200603 0600

---
+++ library/greet.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chapter 8. 모듈
+
+# In[ ]:
+
+
+#예제 8-1
+def hello1(name):
+    x = '%s님 안녕하세요.' %name
+    return x
+def hello2(name):
+    x = '%s님 반갑습니다.' %name
+    return x
+def hello3(name):
+    x = '%s님 만나서 반가워요.' %name
+    return x
+
